05b1f37f0e2dfe4d98ce0a3c2a7df52cbd104444
Austin Stiffler <austin.stiffler@outlook.com>
2021-03-09 11:09:34 -0500

    first exercise commit

---
+++ Exercises/ProgEx07/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Linq;
+using System;
+
+
+namespace ProgEx07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Exercise 7: Roulette");
+            int[] firstCol = new int[] { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
+            int[] secCol = new int[] { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 };
+            int[] thirdCol = new int[] { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 };
+            int[] greens = new int[] { 0, 00 };
+            int[] blacks = new int[] { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+            int[] reds = new int[] { 1, 3, 5, 7, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+            Random num = new Random();
+            int newNum = num.Next(00, 36);
+
+            Console.WriteLine($"The ball fell on {newNum}!");
+            Console.WriteLine("Here are the winning bets..");
+
+            Console.WriteLine($"Numbers: {newNum}");
+            if (newNum % 2 == 0 && newNum != 0 && newNum != 00)
+                Console.WriteLine("Evens/Odds: Evens");
+            else if (newNum % 2 == 1)
+                Console.WriteLine("Evens/Odds: Odds");
+
+            if (reds.Contains(newNum))
+                Console.WriteLine("Reds/Blacks: Reds");
+            else if (blacks.Contains(newNum))
+                Console.WriteLine("Reds/Blacks: Blacks");
+
+            if (newNum > 0 && newNum <= 18)
+                Console.WriteLine("Lows/Highs: Lows");
+            else if (newNum >= 19 && newNum <= 36)
+                Console.WriteLine("Lows/Highs: Highs");
+
+            if (0 < newNum && newNum <= 12)
+                Console.WriteLine("Dozens: Low Dozen (1-12)");
+            else if (newNum >= 13 && newNum <= 24)
+                Console.WriteLine("Dozens: Middle Dozen (13-24)");
+            else if (newNum >= 25 && newNum <= 36)
+                Console.WriteLine("Dozens: High Dozen (25-36)");
+
+            if (firstCol.Contains(newNum))
+            {
+                Console.WriteLine("Columns: First Column");
+                Console.WriteLine($"Street: {newNum}, {newNum + 1}, {newNum + 2}");
+                if (newNum == 1)
+                    Console.WriteLine("Split: 1, 2 or 1, 4");
+                else if (newNum == 34)
+                    Console.WriteLine("Split: 34, 35 or 34, 31");
+                else
+                    Console.WriteLine($"Split: {newNum}, {newNum - 3} or {newNum}, {newNum + 1} or {newNum}, {newNum + 3}");
+
+            }
+            else if (secCol.Contains(newNum))
+            {
+                Console.WriteLine("Columns: Second Column");
+                Console.WriteLine($"Street: {newNum - 1}, {newNum}, {newNum + 1}");
+                if (newNum == 2)
+                    Console.WriteLine("Split: 1, 2 or 2, 3 or 2, 5");
+                else if (newNum == 35)
+                    Console.WriteLine("Split: 34, 35 or 35, 36 or 35, 32");
+                else
+                    Console.WriteLine($"Split: {newNum}, {newNum - 1} or {newNum}, {newNum + 1} or {newNum}, {newNum + 3} or {newNum}, {newNum - 3}");
+            }
+            else if (thirdCol.Contains(newNum))
+            {
+                Console.WriteLine("Columns: Third Column");
+                Console.WriteLine($"Street: {newNum - 2}, {newNum - 1}, {newNum}");
+                if (newNum == 3)
+                    Console.WriteLine("Split: 2, 3 or 3, 6");
+                else if (newNum == 36)
+                    Console.WriteLine("Split: 35, 36 or 33, 36");
+                else
+                    Console.WriteLine($"Split: {newNum}, {newNum - 3} or {newNum}, {newNum - 1} or {newNum}, {newNum + 3}");
+            }
+
+            if (firstCol.Contains(newNum) && newNum == 1)
+                Console.WriteLine("Corners: 1, 2, 5, 4");
+            else if (firstCol.Contains(newNum) && newNum == 34)
+                Console.WriteLine("Corners: 34, 31, 32, 35");
+            else if (firstCol.Contains(newNum) && newNum != 1 && newNum != 34)
+                Console.WriteLine($"Corners: {newNum}, {newNum - 3}, {newNum - 2}, {newNum + 1} or {newNum}, {newNum + 1}, {newNum + 4}, {newNum + 3}");
+
+            if (secCol.Contains(newNum) && newNum == 2)
+            {
+                Console.WriteLine("Corners: 1, 2, 4, 5 or 2, 3, 5, 6");
+            }
+            else if (secCol.Contains(newNum) && newNum == 35)
+            {
+                Console.WriteLine("Corners: 31, 32, 34, 35 or 32, 33, 35, 36");
+            }
+            else if (secCol.Contains(newNum) && newNum != 2 && newNum != 35)
+            {
+                Console.WriteLine($"Corners: {newNum - 4}, {newNum - 3}, {newNum}, {newNum - 1} or {newNum - 3}, {newNum - 2}, {newNum}, {newNum + 1} or {newNum + 1}, {newNum + 4}, {newNum}, {newNum + 3} or {newNum - 1}, {newNum}, {newNum + 3}, {newNum + 2}");
+            }
+
+            if (thirdCol.Contains(newNum) && newNum == 3)
+                Console.WriteLine("Corners: 2, 3, 5, 6");
+            else if (thirdCol.Contains(newNum) && newNum == 36)
+                Console.WriteLine("Corners: 32, 33, 35, 36");
+            else if (thirdCol.Contains(newNum) && newNum != 3 && newNum != 36)
+                Console.WriteLine($"Corners: {newNum - 4}, {newNum - 3}, {newNum - 1}, {newNum} or {newNum - 1}, {newNum}, {newNum + 2}, {newNum + 3}");
+
+            if (firstCol.Contains(newNum) && newNum == 1)
+                Console.WriteLine("6 Numbers: 1, 2, 3, 4, 5, 6");
+            else if (firstCol.Contains(newNum) && newNum == 34)
+                Console.WriteLine("6 Numbers: 31, 32, 33, 34, 35, 36");
+            else if (firstCol.Contains(newNum) && newNum != 1 && newNum != 34)
+                Console.WriteLine($"6 Numbers: {newNum - 3}, {newNum - 2}, {newNum - 1}, {newNum}, {newNum + 1}, {newNum + 2} or { newNum}, { newNum + 1}, { newNum + 2}, { newNum + 3}, { newNum + 4}, { newNum + 5}");
+
+            if (secCol.Contains(newNum) && newNum == 2)
+                Console.WriteLine("6 Numbers: 1, 2, 3, 4, 5, 6");
+            else if (secCol.Contains(newNum) && newNum == 35)
+                Console.WriteLine("6 Numbers: 31, 32, 33, 34, 35, 36");
+            else if (secCol.Contains(newNum) && newNum != 2 && newNum != 35)
+                Console.WriteLine($"6 Numbers: {newNum - 4}, {newNum - 3}, {newNum - 2}, {newNum - 1}, {newNum}, {newNum + 1} or {newNum - 1}, {newNum}, {newNum + 1}, {newNum + 2}, {newNum + 3}, {newNum + 4}");
+
+            if (thirdCol.Contains(newNum) && newNum == 3)
+                Console.WriteLine("6 Numbers: 1, 2, 3, 4, 5, 6");
+            else if (thirdCol.Contains(newNum) && newNum == 36)
+                Console.WriteLine("6 Numbers: 31, 32, 33, 34, 35, 36");
+            else if (thirdCol.Contains(newNum) && newNum != 3 && newNum != 36)
+                Console.WriteLine($"6 Numbers: {newNum - 5}, {newNum - 4}, {newNum - 3}, {newNum - 2}, {newNum - 1}, {newNum} or {newNum - 2}, {newNum - 1}, {newNum}, {newNum + 1}, {newNum + 2}, {newNum + 3}");
+
+        }
+    }
+}
---
+++ Exercises/ProgEx08/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ProgEx08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming Exercise 08: Guess My Number Game");
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Part 1: Computer picks a number between 1 - 1000.");
+            Console.WriteLine("Guess the number, human! You get 10 guesses to be correct.");
+
+
+            Random rNum = new Random();
+            int rn = rNum.Next(1, 1000);
+
+            static int humanGuess()
+            {
+                int myGuess = 0;
+                try
+                {
+                    myGuess = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter an integer");
+                    myGuess = humanGuess();
+                }
+                return myGuess;
+            }
+
+            int maxGuesses = 10;
+            int guesses = 0;
+
+            while (true)
+            {
+                int guess = humanGuess();
+                guesses++;
+                if (guess == rn)
+                {
+                    Console.WriteLine("Your guess was correct!");
+                    break;
+                }
+                if (guess > rn)
+                {
+                    Console.WriteLine("Your guess was too high");
+                }
+                if (guess < rn)
+                {
+                    Console.WriteLine("Your guess was too low");
+                }
+                if (guesses == maxGuesses)
+                {
+                    Console.WriteLine($"You lose. The number was: {rn}");
+                    break;
+                }
+                Console.Write($"You have {maxGuesses - guesses} tries left. Enter another number: ");
+            }
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Part 2: Human picks a number between 1 - 100.");
+            Console.WriteLine("Guess the number, computer! You get 7 guesses to be correct.");
+
+            int min = 0; 
+            int max = 100;
+            int middle = 50;
+            string input;
+            int response;
+            int maxG = 7;
+            int g = 0;
+
+            Console.WriteLine($"Is your guess {middle}? (0) = Yes | (1) = Too high | (2) = Too low");
+            input = Console.ReadLine();
+            response = int.Parse(input);
+
+            do
+            {
+                g += 1;
+                if (response == 2)
+                {
+                    min = middle + 1;
+                }
+                else if (response == 1)
+                {
+                    max = middle - 1;
+                }
+                else if (response != 1 && response != 2 && response != 0)
+                {
+                    Console.WriteLine($"{response}");
+                    Console.WriteLine("Please select a valid key (0, 1, or 2).");
+                }
+                else if (response == 0)
+                {
+                    Console.WriteLine("Computer wins!");
+                    break;
+                }
+                middle = (min + max) / 2;
+                Console.WriteLine($"Is your guess {middle}? (0) = Yes | (1) = Too high | (2) = Too low");
+
+                Console.WriteLine($"Computer has {maxG - g} tries left.");
+
+                input = Console.ReadLine();
+                response = int.Parse(input);
+
+            } while (g < maxG - 1 && response != 0);
+
+            Console.WriteLine("Game over.");
+        }
+    }
+}
